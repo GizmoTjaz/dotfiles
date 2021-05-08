da991ab18490e83b05c7de735beb6290d8ad1c8b
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-05-08 16:22:43 +0200

    ll and lt aliases

---
+++ linux/.config/fish/functions/ll.fish
@@ -0,0 +1,3 @@
+function ll
+	exa --classify --all --group-directories-first --sort=Name --icons --tree --level=1
+end
---
+++ linux/.config/fish/functions/lt.fish
@@ -0,0 +1,3 @@
+function lt
+	exa --classify --all --group-directories-first --sort=Name --icons --tree --level=2 --ignore-glob="node_modules|.git"
+end
