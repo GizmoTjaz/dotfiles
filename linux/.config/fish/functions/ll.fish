c7908b285963d272cd65482a1031384902156259
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-05-10 10:04:02 +0200

    put aliases into fish config

--- linux/.config/fish/functions/ll.fish
+++
@@ -1,3 +0,0 @@
-function ll
-	exa --classify --all --group-directories-first --sort=Name --icons --tree --level=1
-end
--- linux/.config/fish/functions/lt.fish
+++
@@ -1,3 +0,0 @@
-function lt
-	exa --classify --all --group-directories-first --sort=Name --icons --tree --level=2 --ignore-glob="node_modules|.git"
-end
